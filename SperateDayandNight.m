669dd86c655c9c1c8e2ffbcfb6674207ce962e0a
Firepanda415 <dzhengmq@gmail.com>
2017-01-19 17:04:23 -0500

    22

--- SperateDayandNight.m
+++ SperateDayandNight.m
@@ -3,7 +3,7 @@ function [day_rngmap,night_rngmap]=SperateDayandNight(d)
 
 d='ROF_CODAR_20160502_4350_ch0.mat';
 load(d);
-time=min(find(rngmap(size(rngmap,1),:)< mean(rngmap(size(rngmap,1),:))))%The number of column when night starts
+time=min(find(rngmap(size(rngmap,1),:)< mean(rngmap(size(rngmap,1),:))));%The number of column when night starts
 day_rngmap=rngmap(1:size(rngmap,1),1:time);
 night_rngmap=rngmap(1:size(rngmap,1),time:size(rngmap,2));
 % figure;
