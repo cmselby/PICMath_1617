091f670d4ffb9f607ce5c1f72059119a67630804
Muqing Zheng <dzhengmq@gmail.com>
2017-04-17 17:06:49 -0400

    Generate half_max picture

--- A_Trial_(Day_Part_is_Not_Good)/main.m
+++ A_Trial_(Day_Part_is_Not_Good)/main.m
@@ -1,15 +1,11 @@
-function main(nightcof,daycof)
-%Default nightcof = 0.8 daycof = 0.9
-clear;
-clc;
-close all;
+clc;close all;clear;
 %Initialize variables
 mintime = 15; %minimum time last for a track (unit:minutes)
 d='ROF_CODAR_20160502_4350_ch0.mat';
 time1 = 620; %When evening starts
 time2 = 1380; %When morning starts
-% nightcof = 0.8; %Use for threshold, please see "TrackExtraction.m"
-% daycof = 0.9; %Use for threshold, please see "TrackExtraction.m"
+nightcof = 0.8; %Use for threshold, please see "TrackExtraction.m"
+daycof = 0.9; %Use for threshold, please see "TrackExtraction.m"
 
 load(d);
 k = zeros(size(rngmap));
@@ -37,7 +33,17 @@ k(:,time2+1:end) = k_dawn;
 
 [table,picture] = TrackOutput(k,rngmap,mintime);
 
-save('TrackExtraction.mat','picture','table','t','rngmap');
+half_max = zeros(size(picture));
+for row = 1:(size(table,1)/3)
+    col = 1;
+    p = 3*row - 2;
+    while (col<= size(table,2) && (table(p,col) ~=0))
+        half_max([table(p+1,col):table(p+2,col)],table(p,col))=1;
+        col = col + 1;
+    end
+end
+
+save('TrackExtraction.mat','picture','table','rngmap','half_max');
 
 %Display input data and final results
 figure;
@@ -49,3 +55,8 @@ figure;
 imshow(picture);
 set(gca,'YDir','norm'); 
 title('Result');
+
+figure;
+imshow(half_max);
+set(gca,'YDir','norm'); 
+title('Half-Max Result');
