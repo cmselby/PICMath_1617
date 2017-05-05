043d9471014be64d61c6b45d37b92dc4ea22f9b2
Muqing Zheng <dzhengmq@gmail.com>
2017-05-05 17:42:39 -0400

    New version

---
+++ Final_Program/TrackOutput.m
@@ -0,0 +1,65 @@
+function [table,picture] = TrackOutput(current,original,mintime)
+%function [table,picture] = TrackOutput(current,original,mintime)
+%Output the final image of track a table of track information
+%Inputs:
+%current - current result
+%original - original data
+%mintime - minmum time for a track
+%Outputs
+%table - a table of track information that contains time and range
+%picture - the picture contain all tracks
+
+CC = bwconncomp(current);
+numPixels = cellfun(@numel,CC.PixelIdxList);
+%Get a list of tracks
+[biggest,idx] = find(numPixels>1);
+otrack = 0;
+
+%Separate each tracks into independent picture
+for id=1:size(idx,2)
+    sp = zeros(size(current));
+    for in=1:size(idx,2)
+        if in == id
+            sp(CC.PixelIdxList{idx(in)}) = 1;
+        end
+    end
+    stt = regionprops(sp,'PixelList');
+    timerange = unique(stt.PixelList(:,1));
+    %See if the track is greater than a given time length, e.g. 15 miniutes
+    if size(timerange,1) >= mintime
+        otrack = otrack + 1;
+        res(:,:,otrack) = sp;
+    end
+end
+
+if ~exist('res')
+    res = zeros(size(current));
+    table = zeros(3,1);
+end
+
+%Find range of a track in a given time
+if (otrack>0)
+    for ind = 1:size(res,3)
+        st = regionprops(res(:,:,ind),'PixelList');
+        timerange = unique(st.PixelList(:,1));
+        for tm = 1:size(timerange,1)
+            [heightrange] = find(st.PixelList(:,1)==(timerange(tm)));
+
+            hr = st.PixelList(heightrange,2); 
+            ave = mean(original(:,tm));        
+            data = original(hr,tm);
+            maxi = max(original(hr,tm));
+            lowerbound = maxi - abs((maxi - ave))/2;%set a zero mean
+
+            [indi] = find(data>lowerbound);
+            hr = hr(indi);
+            %In the table, tree consecutive lines describ a track
+            table(3*(ind-1)+1,tm) = timerange(tm);%First line shows the time of track (unit minutes since 00:00 UTC)
+            table(3*(ind-1)+2,tm) = min(hr);%Second line shows the lowerbound of height of the track
+            table(3*(ind-1)+3,tm) = max(hr);%Third line shows the upperbound of height of the track;
+        end
+    end
+end
+
+picture = sum(res,3);
+end
---
+++ Final_Program/main.m
@@ -0,0 +1,17 @@
+clear;close all;clc;
+%Set data
+data = 'ROF_CODAR_20160502_4350_ch0.mat';
+%Extrack tracks (Please see 'TrackExtraction.m' for explaination of arguments)
+TrackExtraction(data,0.91,0.80,1380,620,15);
+
+%Load the result
+name= strsplit(data,'.');
+res_name = strcat(strcat('Track_',name(1)),'.mat');
+load(char(res_name));
+
+%Show one of the result
+figure;
+imshow(half_max);
+set(gca,'YDir','norm'); 
+xlabel('Universal Time (minutes)');
+ylabel('Height (km)');
